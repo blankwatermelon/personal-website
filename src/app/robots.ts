6ceb0c5bcd3ee65f2e6ba7a7c32ff2a999ec62f1
Kenney Tran <kenneytran123@gmail.com>
2025-12-04 04:23:52 -0500

    Update URLs to Vercel domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://www.vipuljha.com";
+  const baseUrl = "https://personal-website-two-pi-35.vercel.app";
 
   return {
     rules: {
